c2e763c1c7505fd0c303b985c44e385dec8073c5
Laura <laura.barona@uao.edu.co>
2025-05-12 11:55:50 -0500

    search location hu4

--- src/app/core/services/location.service.ts
+++ src/app/core/services/location.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { Location } from '../../shared/models/location.model';
 import { Department } from '../../shared/models/department.model';
 import { City } from '../../shared/models/city.model';
 import { LocationSearch } from '../../shared/models/location-search.model';
 import { PageResult } from '../../shared/models/page-result.model';
+
 import { environment } from 'src/environments/environment';
+import { DEFAULT_PAGINATION } from '../../shared/constants/pagination.constants';
+import { buildPaginationParams } from '../../shared/utils/http-params.util';
 
 @Injectable({
   providedIn: 'root',
@@ -30,21 +34,17 @@ export class LocationService {
 
   searchLocations(
     searchTerm: string = '',
-    page: number = 0,
-    size: number = 10,
+    page: number = DEFAULT_PAGINATION.PAGE,
+    size: number = DEFAULT_PAGINATION.SIZE,
     sortBy: string = 'city.name',
     sortDirection: string = 'asc'
   ): Observable<PageResult<LocationSearch>> {
-
-    let params = new HttpParams()
+    let params = buildPaginationParams(page, size);
+    params = params
       .set('searchTerm', searchTerm)
-      .set('page', page.toString())
-      .set('size', size.toString())
       .set('sortBy', sortBy)
       .set('sortDirection', sortDirection);
 
-    const searchEndpoint = `${this.apiUrl}/location/search`;
-
-    return this.http.get<PageResult<LocationSearch>>(searchEndpoint, { params });
+    return this.http.get<PageResult<LocationSearch>>(`${this.apiUrl}/location/search`, { params });
   }
 }
