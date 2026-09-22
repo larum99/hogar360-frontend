7e0c100f48b09659f590fa4699de91ce73b2fec2
Laura <laura.barona@uao.edu.co>
2026-09-22 18:29:49 -0500

    setup to prod - environments

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    housesApiUrl: 'https://hogar360-houses-ms.onrender.com/api/v1',
+    usersApiUrl: 'https://hogar360-users-ms.onrender.com/api/v1',
+    visitsApiUrl: 'https://hogar360-visits-ms.onrender.com/api/v1',
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    housesApiUrl: 'http://localhost:8090/api/v1',
+    usersApiUrl: 'http://localhost:8091/api/v1',
+    visitsApiUrl: 'http://localhost:8092/api/v1',
+};
